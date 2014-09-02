8f30aa0b7a0a7cec3b6edd94018f850a6c498974
pranav-jha <jha.pranav.s@gmail.com>
2014-09-02 15:44:01 +0530

    migrated to mocha and chai
    migrated to mocha and chai

--- test/index.js
+++ test/index.js
@@ -1,8 +1,9 @@
 'use strict';
-var resolver = require('./injector')();
+var resolver = require('../lib'),
+    expect = require('chai').expect;
 describe('base.resolver', function () {
     // it should return a module
     it('it should return a module', function () {
-        expect(resolver).not.toBe(undefined);
+        expect(resolver).not.to.equal(undefined);
     });
 });
