c172cc76cdb7bb3a70f0d8bc1972bf88b698e840
pranav-jha <jha.pranav.s@gmail.com>
2014-09-05 17:10:40 +0530

    fixed coveralls

--- test/index.js
+++ test/index.js
@@ -1,23 +1,20 @@
 'use strict';
-var expect = require('chai').expect;
+var resolver = require('../lib'),
+    expect = require('chai').expect;
 describe('base.resolver', function () {
-    beforeEach(function () {
-        this.resolver = require('../lib');
-    });
     // it should return a module
     it('should return a module', function () {
-        expect(this.resolver).not.to.equal(undefined);
+        expect(resolver).not.to.equal(undefined);
     });
     // it should throw an error if configured with a non existant file
     it('should throw an error if configured with a non existant file', function () {
-        var resolver = this.resolver;
         expect(function () {
             resolver('./test/data/configs/non-existant-path');
         }).to.throw(Error);
     });
     // it should configure properly with an existing file
     it('should configure properly with an existing file', function () {
-        expect(this.resolver('./test/data/configs/configuration-empty-array')).to.have.keys([
+        expect(resolver('./test/data/configs/configuration-empty-array')).to.have.keys([
             'load',
             'unload',
             'reload'
@@ -25,7 +22,6 @@ describe('base.resolver', function () {
     });
     // it should not configure properly with a non array
     it('should not configure properly with a non array', function () {
-        var resolver = this.resolver;
         expect(function () {
             return resolver('./test/data/configs/configuration-object');
         }).to.throw(Error);
